f3e63d3208e2d4505f18b1c5b961abe26552ed99
Dmitry Jum <xfiler@ymail.com>
2019-10-03 00:54:33 -0400

    fixes typo

--- concurrency_throttle.js
+++ concurrency_throttle.js
@@ -14,7 +14,7 @@ class Queue {
   }
 
   execute(first_job, last_job) {
-    this.jobs_splice(first, last).forEach((j) => { //takes given count of async jobs and executes them immidietly and cuts them from the queue
+    this.jobs.splice(first, last).forEach((j) => { //takes given count of async jobs and executes them immidietly and cuts them from the queue
       j().then(() => this.execute(0, 1)) //as soon as any of the looped requests succeeds, it'll cut the first available request in the queue and execute it
     })
   }
